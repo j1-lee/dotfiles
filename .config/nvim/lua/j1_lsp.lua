072de68a9d40782bb12aeb3ab2e9bca1f6c43e90
Jaewon Lee <jaewon.lee.0209@gmail.com>
2022-02-22 16:25:27 -0500

    feat: add some keymaps for lsp

--- .config/nvim/lua/j1_lsp.lua
+++ .config/nvim/lua/j1_lsp.lua
@@ -9,8 +9,16 @@ cmp.setup {
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
+local on_attach = function(_, bufnr)
+  local nmap_buf = function(lhs, rhs)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', lhs, rhs, {silent = true})
+  end
+  nmap_buf('<Leader>e', '<Cmd>lua vim.diagnostic.open_float()<CR>')
+  nmap_buf('K', '<Cmd>lua vim.lsp.buf.hover()<CR>')
+end
+
 local lspconfig = require 'lspconfig'
 local servers = {'pyright'}
 for _, server in ipairs(servers) do
-  lspconfig[server].setup {capabilities = capabilities}
+  lspconfig[server].setup {capabilities = capabilities, on_attach = on_attach}
 end
