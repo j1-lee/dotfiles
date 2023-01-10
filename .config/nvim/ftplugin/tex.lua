60963168284169e776d91d24e8c045636f290f58
Jaewon Lee <jaewon.lee.0209@gmail.com>
2023-01-10 15:37:15 -0500

    feat: add tex setting for nvim-surround

--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -1,4 +1,8 @@
 vim.opt_local.spell = true
 
-vim.b.surround_39 = "`\r'" -- ys<motion>'
-vim.b.surround_34 = "``\r''" -- ys<motion>"
+require('nvim-surround').buffer_setup {
+  surrounds = {
+    ['"'] = { add = { "``", "''" } },
+    ["'"] = { add = { "`", "'" } },
+  }
+}
