2a8f3ff924207426e4cbbad79c93bba70ed7d904
Jaewon Lee <jaewon.lee.0209@gmail.com>
2023-01-10 21:27:36 -0500

    feat: configure matchit for matlab

--- .config/nvim/ftplugin/matlab.lua
+++ .config/nvim/ftplugin/matlab.lua
@@ -1,3 +1,7 @@
 vim.opt_local.tabstop = 4 -- # of visual spaces per tab
 vim.opt_local.softtabstop = 4 -- # of spaces in tab when editing
 vim.opt_local.shiftwidth = 4 -- # of spaces for indentation (when using >>)
+
+local rx_start = [[\<classdef\>\|\<methods\>\|\<function\>\|\<if\>\|\<for\>\|\<while\>\|\<switch\>\|\<try\>]]
+local rx_end = [[\(([^)]*\)\@<!\<end\>\([^(]*)\)\@!]]
+vim.b.match_words = rx_start .. ':' .. rx_end
