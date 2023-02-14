d75559108ff450e5d81fd54409f2c04956b7ace2
Jaewon Lee <jaewon.lee.0209@gmail.com>
2023-02-14 11:42:25 -0500

    fix: rename sumneko_lua

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -85,7 +85,7 @@ function config.lspconfig()
     capabilities = capabilities,
   }
 
-  lspconfig.sumneko_lua.setup {
+  lspconfig.lua_ls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
     settings = {
