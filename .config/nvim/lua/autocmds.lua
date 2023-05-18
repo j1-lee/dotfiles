2ab42bd4d50daf20049b986cac9848d8091750bc
Jaewon Lee <jaewon.lee.0209@gmail.com>
2023-05-18 16:45:23 -0400

    feat: remove packer autocompile

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,7 +1,6 @@
 local util = require 'util'
 
 local augroup = vim.api.nvim_create_augroup('init_lua', { clear = true })
-
 local function autocmd(event, opts)
   opts.group = augroup
   vim.api.nvim_create_autocmd(event, opts)
@@ -20,11 +19,3 @@ autocmd('WinEnter', { callback = function() vim.opt.cursorline = true end })
 autocmd('WinLeave', { callback = function() vim.opt.cursorline = false end })
 
 autocmd('BufReadPost', { callback = util.last_position_jump })
-
-autocmd('BufWritePost', {
-  pattern = 'plugins.lua',
-  callback = function()
-    vim.cmd.source '<afile>'
-    vim.cmd.PackerCompile()
-  end
-})
